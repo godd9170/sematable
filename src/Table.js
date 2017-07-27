e336e539e74b30755f728bb9dea93ef474bf66a2
Hank Goddard <me@henrygoddard.com>
2017-07-27 12:48:01 -0400

    lint and proptypes

--- src/Table.js
+++ src/Table.js
@@ -17,6 +17,9 @@ const propTypes = {
   styleName: PropTypes.string,
   CheckboxComponent: PropTypes.func,
   NoDataComponent: PropTypes.func,
+  sortAscIconClass: PropTypes.string,
+  sortDescIconClass: PropTypes.string,
+  sortIconClass: PropTypes.string,
 };
 
 class Table extends Component {
@@ -30,9 +33,9 @@ class Table extends Component {
       primaryKey,
       CheckboxComponent,
       NoDataComponent,
-      sortAscIconClass='fa fa-long-arrow-up',
-      sortDescIconClass='fa fa-long-arrow-down',
-      sortIconClass='fa fa-arrows-v',
+      sortAscIconClass = 'fa fa-long-arrow-up',
+      sortDescIconClass = 'fa fa-long-arrow-down',
+      sortIconClass = 'fa fa-arrows-v',
     } = this.props;
 
     const className = this.props.className || 'table-sm table-striped table-hover';
